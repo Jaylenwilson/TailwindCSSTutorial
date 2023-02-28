7431fdcdd6dae9572bf557cba1d7f4ba1a351291
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-27 21:25:11 -0500

    learnt how to import custom fonts in tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      {/* Colors */}
       <h1 className="bg-green-600 text-white border-4 border-blue-400 mb-1">
         Hello world!
     </h1>
@@ -13,6 +14,14 @@ function App() {
       <div>
         <p className="text-xs  lg:text-3xl">Hello World!</p>
       </div>
+
+      {/* Typography */}
+      <h1 className="text-3xl">Title</h1>
+      <h2 className="text-2xl">Title</h2>
+      <h3 className="text-xl">Title</h3>
+      <p className="text-base">regular paragraph</p>
+      <p className="text-sm">description paragraph</p>
+      <p className="note text-xs">A little note</p>
     </div>
   )
 }
