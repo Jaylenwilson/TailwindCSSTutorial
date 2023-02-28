d9d42204db25b754e2308b0c2c04b78c16346179
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-27 20:41:18 -0500

    learnt how to use tailwind sizes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
       <h1 className="bg-green-600 text-white border-4 border-blue-400">
         Hello world!
     </h1>
-      <h1 className="text-yellow-600">
+      <h1 className="text-mygreen">
         Hello world!
     </h1>
-
+      <div>
+        <p className="text-xs  lg:text-3xl">Hello World!</p>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         mygreen: "#49e659"
